144b0fa2c50512aaeeb3f7ae2c0f0e5803f53075
lvshudi <lvshudi119@126.com>
2019-08-25 10:01:23 +0800

    return no ticket message

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -11,15 +11,25 @@ public class ParkingBoy {
     }
 
     public ParkingTicket park(Car car) {
+    	
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
         return parkingTicket;
     }
 
     public Car fetch(ParkingTicket ticket) {
-        return parkingLot.fetchCar(ticket);
+    	if (ticket == null) {
+    		this.lastErrorMessage = "Please provide your parking ticket.";
+    		return null;
+    	}
+    	Car car = parkingLot.fetchCar(ticket);
+    	if (car == null) {
+    		this.lastErrorMessage = "Unrecognized parking ticket.";
+    	}
+        return car;
     }
 
     public String getLastErrorMessage() {
         return lastErrorMessage;
     }
+    
 }
